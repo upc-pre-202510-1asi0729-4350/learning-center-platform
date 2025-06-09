1c85b74a4b1b9c0ef874f05ab5d1165aa6fde721
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-09 12:00:18 -0500

    feat(learning): add course relationship to learning path.

--- src/main/java/com/acme/center/platform/learning/domain/model/aggregates/Course.java
+++ src/main/java/com/acme/center/platform/learning/domain/model/aggregates/Course.java
@@ -1,12 +1,39 @@
 package com.acme.center.platform.learning.domain.model.aggregates;
 
+import com.acme.center.platform.learning.domain.model.valueobjects.LearningPath;
+import com.acme.center.platform.learning.domain.model.valueobjects.TutorialId;
 import com.acme.center.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import org.apache.logging.log4j.util.Strings;
 
 @Getter
 @Entity
 public class Course extends AuditableAbstractAggregateRoot<Course> {
     private String title;
     private String description;
+
+    @Embedded
+    private final LearningPath learningPath;
+
+    public Course() {
+        this.title = Strings.EMPTY;
+        this.description = Strings.EMPTY;
+        learningPath = new LearningPath();
+    }
+
+    public Course(String title, String description) {
+        this.title = title;
+        this.description = description;
+        learningPath = new LearningPath();
+    }
+
+    public void addTutorialToLearningPath(TutorialId tutorialId) {
+        this.learningPath.addItem(this, tutorialId);
+    }
+
+    public void addTutorialToLearningPath(TutorialId tutorialId, TutorialId nextTutorialId) {
+        this.learningPath.addItem(this, tutorialId, nextTutorialId);
+    }
 }
