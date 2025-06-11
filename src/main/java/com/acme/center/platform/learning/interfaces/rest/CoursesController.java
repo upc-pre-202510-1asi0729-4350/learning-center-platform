62ed05f91a81dfa9c90fa993bd7a74b4fc0f08ea
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-11 10:49:07 -0500

    feat(learning): add course controller.

--- src/main/java/com/acme/center/platform/learning/interfaces/rest/CoursesController.java
+++ src/main/java/com/acme/center/platform/learning/interfaces/rest/CoursesController.java
@@ -4,14 +4,16 @@ import com.acme.center.platform.learning.domain.model.queries.GetCourseByIdQuery
 import com.acme.center.platform.learning.domain.services.CourseCommandService;
 import com.acme.center.platform.learning.domain.services.CourseQueryService;
 import com.acme.center.platform.learning.interfaces.rest.resources.CourseResource;
+import com.acme.center.platform.learning.interfaces.rest.resources.CreateCourseResource;
 import com.acme.center.platform.learning.interfaces.rest.transform.CourseResourceFromEntityAssembler;
+import com.acme.center.platform.learning.interfaces.rest.transform.CreateCourseCommandFromResourceAssembler;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -36,5 +38,25 @@ public class CoursesController {
         var courseEntity = course.get();
         var courseResource = CourseResourceFromEntityAssembler.toResourceFromEntity(courseEntity);
         return ResponseEntity.ok(courseResource);
+    }
+
+    @PostMapping
+    @Operation(summary = "Create Course", description = "Create a new course with the provided details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Course created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "404", description = "Course not found")
+    })
+    public ResponseEntity<CourseResource> createCourse(@RequestBody CreateCourseResource resource) {
+        var createCourseCommand = CreateCourseCommandFromResourceAssembler.toCommandFromResource(resource);
+        var courseId = courseCommandService.handle(createCourseCommand);
+        if (courseId == null || courseId == 0L) return ResponseEntity.badRequest().build();
+        var getCourseByIdQuery = new GetCourseByIdQuery(courseId);
+        var course = courseQueryService.handle(getCourseByIdQuery);
+        if (course.isEmpty()) return ResponseEntity.notFound().build();
+        var courseEntity = course.get();
+        var courseResource = CourseResourceFromEntityAssembler.toResourceFromEntity(courseEntity);
+        return new ResponseEntity<>(courseResource, HttpStatus.CREATED);
+
     }
 }
